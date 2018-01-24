97fa19d9dfe16431b83547ac1b8eba2af5b04bad
Vyacheslav Shcherbak <dba@uspekh.org.ua>
2018-01-24 18:30:05 +0100

    Added empty project for Custom Data Flow Component

---
+++ CustomDataFlowComponent/CustomDataFlowComponent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Resources;
+using System.IO;
+using Microsoft.SqlServer.Dts.Pipeline;
+using Microsoft.SqlServer.Dts.Pipeline.Wrapper;
+
+namespace CustomDataFlowComponent
+{
+    [DtsPipelineComponent(DisplayName = "CustomDataFlowComponent", ComponentType = ComponentType.SourceAdapter)]
+    public class XmlDestination : PipelineComponent
+    {
+
+        public override IDTSCustomProperty100 SetComponentProperty(string propertyName, object propertyValue)
+        {
+            return base.SetComponentProperty(propertyName, propertyValue);
+        }
+
+        public override void ProvideComponentProperties()
+        {
+            base.ProvideComponentProperties();
+        }
+
+        public override void AcquireConnections(object transaction)
+        {
+            base.AcquireConnections(transaction);
+        }
+
+        public override void PrimeOutput(int outputs, int[] outputIDs, PipelineBuffer[] buffers)
+        {
+            base.PrimeOutput(outputs, outputIDs, buffers);
+        }
+
+        public override void PreExecute()
+        {
+            base.PreExecute();
+        }
+
+        public override DTSValidationStatus Validate()
+        {
+            return base.Validate();
+        }
+
+    }
+}  
+
